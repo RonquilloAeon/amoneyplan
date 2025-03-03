7cb75193498769ffe0125331bdcc9801e885fb06
Martin R <apurplethinker@gmail.com>
2025-03-03 14:50:18 -0600

    clean up Dockerfile

--- backend/noxfile.py
+++ backend/noxfile.py
@@ -3,7 +3,7 @@ import nox
 nox.options.sessions = ["test"]
 
 
-@nox.session(python="3.10", reuse_venv=True)
+@nox.session(python="3.13", reuse_venv=True)
 def test(session):
     session.install("-r", "test-requirements.txt")
     session.run("pytest", *session.posargs)

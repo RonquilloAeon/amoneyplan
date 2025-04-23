e0f141606865528435ad8158579601638514c363
Martin R <apurplethinker@gmail.com>
2025-04-23 06:30:20 -0500

    fix start, again

--- backend/start.sh
+++ backend/start.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Collect static files
-python src/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply database migrations
-python src/manage.py migrate
+python manage.py migrate
 
 # Start Uvicorn using exec
 exec uvicorn amoneyplan.asgi:application --host 0.0.0.0 --port "${PORT:-8000}"

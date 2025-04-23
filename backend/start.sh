0d4e88716e4cec50af3fe60e7316c13cc9d9dcd6
Martin R <apurplethinker@gmail.com>
2025-04-23 04:53:17 -0500

    add build config

---
+++ backend/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
+
+# Start Uvicorn using exec
+exec uvicorn amoneyplan.asgi:application --host 0.0.0.0 --port "${PORT:-8000}" --no-input
